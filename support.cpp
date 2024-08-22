fa0a70b3835d5ce089dafefdcaa132648c949bac
annpr1 <unidou@yandex.ru>
2024-08-22 14:42:51 +0300

    Add function

--- support.cpp
+++ support.cpp
@@ -2,20 +2,54 @@
 #include <math.h>
 
 #include "support.h"
+#include "Square.h"
 
-
-int Compare_with_null(double n)
+int CompareFloats(double n1, double n2)
 {
-    if (fabs(n) < EPS)
+    if (fabs(n1 - n2) < EPS)
         return 1;
     else
         return 0;
 }
 
-void Clear_board()
+void Clearbuffer()
 {
     while (getchar() != '\n')
     {
         ;
     }
+}
+
+void PrintResult(struct square first)
+{
+    int NumRoots = SquareSolver(first);
+    switch (NumRoots)
+        {
+            case NO_ROOTS:
+
+                printf("No solutions\n");
+
+                break;
+
+
+            case ONE_ROOT:
+
+                printf("x = %lg\n", *first.x1);
+
+                break;
+
+
+            case TWO_ROOTS:
+
+                printf("x1 = %lg, x2 = %lg\n", *first.x1, *first.x2);
+
+                break;
+
+            case INF_ROOTS:
+
+                printf("Any number\n");
+
+                break;
+
+        }
 }
